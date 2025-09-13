696831c86ac8dca5e5b3b9efe6ae55bcce5e0a39
boris2442 <aubinborissimotsebo@gmail.com>
2025-09-13 15:29:08 +0200

    add icone

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,24 @@ import spes from '../assets/projects_images/spes.png';
 import streaming from '../assets/projects_images/streaming.png';
 import { useTranslation } from "react-i18next";
 import ImageWithSkeleton from './ImageWithSkeleton';
+
+
+import { FaReact, FaBolt } from "react-icons/fa";
+import {
+  SiVuedotjs,
+  SiTailwindcss,
+
+  SiLaravel,
+  SiExpress,
+
+  SiNodedotjs,
+  SiMongodb,
+  SiMysql,
+  SiPhp,
+  SiBootstrap,
+
+} from "react-icons/si";
+
 export default function Projects() {
   const { t } = useTranslation();
 
@@ -23,6 +41,12 @@ export default function Projects() {
             <p>{t('projects.chataw.desc')}</p>
             <button> <a href="https://evendeco.com/">{t('projects.chataw.view')}</a> </button>
           </div>
+          <div className="pourcentage-project" >
+            <span>  <FaReact size={30} color="#61dafb" /></span>
+            <span>   <SiTailwindcss size={30} color="#38b2ac" /></span>
+            <span>    <SiMysql size={30} color="#4479A1" /></span>
+            <span>    <SiPhp size={30} color="#4479A1" /></span>
+          </div>
         </div>
         <div data-aos='flip-left'>
           <div className="project-img">
@@ -34,6 +58,20 @@ export default function Projects() {
             <p>{t('projects.quiz.desc')}</p>
             <button> <a href="https://campuspay.evendeco.com/">{t('projects.quiz.view')}</a> </button>
           </div>
+          <div className="pourcentage-project">
+            <span>
+              <SiLaravel size={30} color="#ff2d20" />
+            </span>
+            <span>
+              <SiTailwindcss size={30} color="#38b2ac" />
+            </span>
+            <span>
+              <SiMysql size={30} color="#4479A1" />
+            </span>
+            <span>
+              <FaBolt size={30} color="#8BC0D0" />
+            </span>
+          </div>
         </div>
         <div data-aos='flip-left'>
           <div className="project-img">
@@ -42,8 +80,25 @@ export default function Projects() {
           </div>
           <hr />
           <div className="description">
-            {/* <p>{t('projects.spes.desc')}</p> */}
+            <p>{t('projects.spes.desc')}</p>
             <button> <a href="https://godfashion.evendeco.com/">{t('projects.spes.view')}</a> </button>
+          </div>
+          <div className="pourcentage-project">
+            <span>
+              <SiVuedotjs size={30} color="#41b883" />
+            </span>
+            <span>
+              <SiMongodb size={30} color="#47A248" />
+            </span>
+            <span>
+              <SiNodedotjs size={30} color="#339933" />
+            </span>
+            <span>
+              <SiExpress size={30} color="#000000" />
+            </span>
+            <span>
+              <SiBootstrap color="#7952B3" size={30} />
+            </span>
           </div>
         </div>
         <div data-aos='flip-left'>
@@ -55,6 +110,17 @@ export default function Projects() {
           <div className="description">
             <p>{t('projects.streaming.desc')}</p>
             <button> <a href="https://boristech.evendeco.com">{t('projects.streaming.view')}</a> </button>
+          </div>
+          <div className="pourcentage-project">
+            <span>
+              <SiLaravel size={30} color="#ff2d20" />
+            </span>
+            <span>
+              <SiTailwindcss size={30} color="#38b2ac" />
+            </span>
+            <span>
+              <SiVuedotjs size={30} color="#41b883" />
+            </span>
           </div>
         </div>
       </div>
