7f93c77ab6a53343632d01a3c43c9e7a73a4edd0
boris2442 <aubinborissimotsebo@gmail.com>
2025-09-13 14:51:48 +0200

    add icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,4 @@
 
-// import Navigation from "./Navigation";
-
-// export default function () {
-
-//   const navLikns = ["Home", "About me", "Services", "Experiences", "Projects"];
-
-
-
-//   return (
-//     <header>
-//       <div className="logo">
-//         <span className="first-letter">A</span>
-//         <span>ubin</span>
-//       </div>
-//       <Navigation />
-//       <button><a href="#Contact">Contact me</a></button>
-//     </header>
-//   );
-// }
 import Navigation from "./Navigation";
 import { useTranslation } from "react-i18next";
 // import LanguageSwitcher from "./LanguageSwitcher";
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,65 +1,3 @@
-// import chataw from '../assets/projects_images/chataw.png';
-// import quiz from '../assets/projects_images/quiz.png';
-// import spes from '../assets/projects_images/spes.png';
-// import streaming from '../assets/projects_images/streaming.png';
-
-// export default function () {
-//   return (
-//     <section id="Projects" className="projects">
-//       <h2>My Recents Projects</h2>
-//       <div className="content">
-//         <div data-aos='flip-left'>
-//           <div className="project-img">
-//             <img src={chataw} alt="" srcset="" />
-//           </div>
-//           <hr />
-//           <div className="description">
-//             <p>
-//               A platform dedicated to event planning, offering personalized catering services for weddings, seminars and receptions.
-//             </p>
-//             <button> <a href="https://evendeco.com/">View more</a> </button>
-//           </div>
-//         </div>
-//         <div data-aos='flip-left'>
-//           <div className="project-img">
-//             <img src={quiz} alt="" srcset="" />
-//           </div>
-//           <hr />
-//           <div className="description">
-//             <p>
-//               A student management app that allows users to track payments, courses, and personal academic information efficiently</p>
-//             <button> <a href="https://campuspay.evendeco.com/">View more</a> </button>
-//           </div>
-//         </div>
-//         <div data-aos='flip-left'>
-//           <div className="project-img">
-//             <img src={spes} alt="" srcset="" />
-//           </div>
-//           <hr />
-//           <div className="description">
-//             <p>
-//               A complete management system for God Fashion that streamlines orders, stock, and client interactions, built with Laravel and Tailwind CSS.
-//             </p>
-//             <button> <a href="https://godfashion.evendeco.com/">View more</a> </button>
-//           </div>
-//         </div>
-//         <div data-aos='flip-left'>
-//           <div className="project-img">
-//             <img src={streaming} alt="" srcset="" />
-//           </div>
-//           <hr />
-//           <div className="description">
-//             <p>
-//               An e-commerce platform for Boris Tech that allows users to browse, order, and pay for electronic devices securely, built with Laravel and Tailwind CSS.
-//             </p>
-//             <button> <a href="https://boristech.evendeco.com">View more</a> </button>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
 
 
 import chataw from '../assets/projects_images/chataw.png';
@@ -104,7 +42,7 @@ export default function Projects() {
           </div>
           <hr />
           <div className="description">
-            <p>{t('projects.spes.desc')}</p>
+            {/* <p>{t('projects.spes.desc')}</p> */}
             <button> <a href="https://godfashion.evendeco.com/">{t('projects.spes.view')}</a> </button>
           </div>
         </div>
